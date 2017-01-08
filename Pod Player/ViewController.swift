1284f2993578bee0539d8b74568eb3ad18928b07
Tom Davis <tom@tomdavislx.co.uk>
2017-01-08 16:53:38 +0000

    Initial Commit

---
+++ Pod Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pod Player
+//
+//  Created by Tom Davis on 08/01/2017.
+//  Copyright © 2017 Tom Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
